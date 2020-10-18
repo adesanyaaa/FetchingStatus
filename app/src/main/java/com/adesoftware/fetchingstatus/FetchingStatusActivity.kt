0564e79afbf42301beca7b426382c25bab4f7f9d
AndroidAde <adesanyaaa@gmail.com>
2020-10-18 17:50:53 +0100

    Consume JSON data

---
+++ app/src/main/java/com/adesoftware/fetchingstatus/FetchingStatusActivity.kt
@@ -0,0 +1,12 @@
+package com.adesoftware.fetchingstatus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FetchingStatusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fetching_status)
+    }
+}
